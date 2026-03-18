8cd357df731aead10e605190cd3f9113501d5719
HLong <truongvohoanglong@gmail.com>
2026-03-18 22:55:00 +0700

    [Feat] Add database services, SQL schema, DB files and mock datas
    - Implementing database structure, query used for initializing is InitDB.sql
    - Mock data implemented by MockData.sql
    - Creating class file for each and every table in the database.
    - Setup connection string in DatabaseHelper.cs and respective ExecuteQuere and ExecuteNonQuery functions
    - Modifying .gitignore so database data can be checked by collaborators.

---
+++ Services/ThamSo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_StudentManagement_Project.Services
+{
+    internal class ThamSo
+    {
+        public required string MaThamSo { get; set; }
+        public string? TenThamSo { get; set; } // Use '?' if it's okay to be null
+        public double GiaTri { get; set; }
+    }
+}
